e38d5b66e8d84d357e9002b78d766da0ebcfdbef
Jonathan <jonathanmeaney1982@gmail.com>
2021-02-01 19:50:10 +0000

    add dynamic ingr/steps inputs to form

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,8 @@
- $(document).ready(function(){
-    $('.sidenav').sidenav();
-    $(".dropdown-trigger").dropdown();
+$(document).ready(function () {
+    $(".add-ingr").click(function () {
+        $(".ingredient").append(' <input class="form-control form-control-md shadow" name="ingredient" type="text" placeholder="Ingredient"> ');
+    });
+     $(".add-step").click(function () {
+        $(".step").append(' <input class="form-control form-control-md shadow" name="step" type="text" placeholder="Step"> ');
+    });
 });
