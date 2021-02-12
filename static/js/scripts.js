525be0de56b4ea8671c2aae6df6158ed5ef13897
Jonathan <jonathanmeaney1982@gmail.com>
2021-02-12 08:14:23 +0000

    Add button to remove step/ingr input

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
+
     $(".add-ingr").click(function () {
-        $(".ingredient").append(' <input class="form-control form-control-md shadow mb-1" name="ingredients" type="text" minlength="3" placeholder="Ingredient"> ');
+        $(".ingredient").append(' <input class="form-control form-control-md shadow mb-1 ingredient-input" name="ingredients" type="text" minlength="3" placeholder="Ingredient"> ');
     });
+
     $(".add-step").click(function () {
-        $(".step").append(' <input class="form-control form-control-md shadow mb-1" name="steps" type="text" minlength="3" placeholder="Step"> ');
+        $(".step").append(' <input class="form-control form-control-md shadow mb-1 step-input" name="steps" type="text" minlength="3" placeholder="Step"> ');
     });
+
+    $(".remove-ingr").click(function () {
+        $(".ingredient-input").last().remove()
+    });
+
+     $(".remove-step").click(function () {
+        $(".step-input").last().remove()
+    });
+
     $(".delete-btn").click(function() {
         $(this).next().show()
     });
+
     $(".cancel-delete-btn").click(function() {
         $(this).parent().hide()
     });
