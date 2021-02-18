c4a22df902dfe2694b0188a91137213767be9da0
Jonathan <jonathanmeaney1982@gmail.com>
2021-02-18 14:37:35 +0000

    Code cleanup edit_recipe.html

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
     $(".add-ingr").click(function () {
-        $(".ingredient").append(' <input class="form-control form-control-md shadow mb-1 ingredient-input" name="ingredients" type="text" minlength="3" placeholder="Ingredient"> ');
+        $(".ingredient").append(' <input class="form-control form-control-md shadow mb-1 ingredient-input" name="ingredients" type="text" minlength="3" aria-label="step input" placeholder="Ingredient"> ');
     });
 
     $(".add-step").click(function () {
-        $(".step").append(' <input class="form-control form-control-md shadow mb-1 step-input" name="steps" type="text" minlength="3" placeholder="Step"> ');
+        $(".step").append(' <input class="form-control form-control-md shadow mb-1 step-input" name="steps" type="text" minlength="3" aria-label="step input" placeholder="Step"> ');
     });
 
     $(".remove-ingr").click(function () {
