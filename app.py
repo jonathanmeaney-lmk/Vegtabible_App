c790de142e2cd8f8335678517126d8a88c6f15e2
Jonathan <jonathanmeaney1982@gmail.com>
2021-02-23 14:00:44 +0000

    Add register.html

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,11 @@ mongo = PyMongo(app)
 def index():
     categories = list(mongo.db.categories.find())
     return render_template("index.html", categories=categories)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 @app.route("/search_page")
